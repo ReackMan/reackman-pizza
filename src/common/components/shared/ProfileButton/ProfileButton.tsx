4d8a3e8aacb51bb1dd9a1c40542bffb6be18cd62
ReackMan <reackman@yandex.ru>
2025-10-21 01:24:06 +0300

    add shared ProfileButton component

---
+++ src/common/components/shared/ProfileButton/ProfileButton.tsx
@@ -0,0 +1,31 @@
+import { useSession } from 'next-auth/react'
+import { Button } from '@/common'
+import { CircleUser, User } from 'lucide-react'
+import Link from 'next/link'
+
+type Props = {
+  onClickSignIn?: () => void
+  className?: string
+}
+
+export const ProfileButton = ({ className, onClickSignIn }: Props) => {
+  const { data: session } = useSession()
+
+  return (
+    <div className={className}>
+      {!session ? (
+        <Button onClick={onClickSignIn} variant={'outline'} className={'flex items-center gap-1'}>
+          <User size={16} />
+          Войти
+        </Button>
+      ) : (
+        <Link href={'/profile'}>
+          <Button variant={'secondary'} className={'flex items-center gap-2'}>
+            <CircleUser size={18} />
+            Профиль
+          </Button>
+        </Link>
+      )}
+    </div>
+  )
+}
