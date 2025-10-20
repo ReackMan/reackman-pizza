7724b9c11d2fbcc133372d2e763dbff44ae09b4c
ReackMan <reackman@yandex.ru>
2025-10-21 01:46:39 +0300

    add checkout api

---
+++ src/app/api/checkout/callback/route.ts
@@ -0,0 +1,48 @@
+import { CartItemDTO, OrderSuccessTemplate, PaymentCallbackData, sendEmail } from '@/common'
+import { OrderStatus } from '@prisma/client'
+import { NextRequest, NextResponse } from 'next/server'
+import { prisma } from '@/prisma/prismaClient'
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = (await req.json()) as PaymentCallbackData
+
+    const order = await prisma.order.findFirst({
+      where: {
+        id: Number(body.object.metadata.order_id),
+      },
+    })
+
+    if (!order) {
+      return NextResponse.json({ error: 'Order not found' })
+    }
+
+    const isSucceeded = body.object.status === 'succeeded'
+
+    await prisma.order.update({
+      where: {
+        id: order.id,
+      },
+      data: {
+        status: isSucceeded ? OrderStatus.SUCCEEDED : OrderStatus.CANCELLED,
+      },
+    })
+
+    const items = JSON.parse(order?.items as string) as CartItemDTO[]
+
+    if (isSucceeded) {
+      await sendEmail(
+        order.email,
+        'Reack Pizza / Ваш заказ успешно оформлен 🎉',
+        OrderSuccessTemplate({ orderId: order.id, items })
+      )
+    } else {
+      // Письмо о неуспешной оплате
+    }
+
+    return NextResponse.json({ message: 'Callback processed' }, { status: 200 })
+  } catch (error) {
+    console.error('[Checkout Callback] Error:', error)
+    return NextResponse.json({ error: 'Server error' })
+  }
+}
