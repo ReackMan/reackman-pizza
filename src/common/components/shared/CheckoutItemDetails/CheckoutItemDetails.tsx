d3d36fbf6954a16b19f6ffafb50708ee2456c6dd
ReackMan <reackman@yandex.ru>
2025-10-21 01:13:59 +0300

    add shared CheckoutItemDetails component

---
+++ src/common/components/shared/CheckoutItemDetails/CheckoutItemDetails.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react'
+import { cn } from '@/common'
+
+type Props = {
+  title?: ReactNode
+  value?: ReactNode
+  className?: string
+}
+
+export const CheckoutItemDetails = ({ title, value, className }: Props) => {
+  return (
+    <div className={cn('flex my-4', className)}>
+      <span className={'flex flex-1 text-lg text-neutral-500'}>
+        {title}
+        <div
+          className={'flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2'}
+        />
+      </span>
+
+      <span className={'font-bold text-lg'}>{value}</span>
+    </div>
+  )
+}
