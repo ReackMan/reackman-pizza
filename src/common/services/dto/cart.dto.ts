c654d7b1b56d0ba1f87773eed82dde749be2b89c
ReackMan <reackman@yandex.ru>
2025-10-21 01:40:29 +0300

    add dto service

---
+++ src/common/services/dto/cart.dto.ts
@@ -0,0 +1,17 @@
+import { Cart, CartItem, Ingredient, Product, ProductVariation } from '@prisma/client'
+
+export type CartItemDTO = CartItem & {
+  productVariation: ProductVariation & {
+    product: Product
+  }
+  ingredients: Ingredient[]
+}
+
+export type CartDTO = Cart & {
+  items: CartItemDTO[]
+}
+
+export type CreateCartItemValues = {
+  productVariationId: number
+  ingredients?: number[]
+}
