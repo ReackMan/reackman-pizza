1ecdf60c83541fcb9bf053c782a7435f3d5b32a6
ReackMan <reackman@yandex.ru>
2025-10-21 01:15:24 +0300

    add shared ChooseProductForm component

---
+++ src/common/components/shared/ChooseProductForm/ChooseProductForm.tsx
@@ -0,0 +1,50 @@
+import { Button, cn, Title } from '@/common'
+import Image from 'next/image'
+
+type Props = {
+  imageUrl: string
+  name: string
+  price: number
+  loading?: boolean
+  onSubmit?: VoidFunction
+  className?: string
+}
+
+/**
+ * Форма выбора ПРОДУКТА
+ */
+export const ChooseProductForm = ({
+  name,
+  imageUrl,
+  price,
+  onSubmit,
+  className,
+  loading,
+}: Props) => {
+  return (
+    <div className={cn(className, 'flex flex-1')}>
+      <div className={'flex items-center justify-center flex-1 relative w-full'}>
+        <Image
+          src={imageUrl}
+          alt={name}
+          style={{ width: 350, height: 350 }}
+          className={'relative left-2 top-2 transition-all z-10 duration-300'}
+          width={350}
+          height={350}
+        />
+      </div>
+
+      <div className={'w-[490px] bg-[#f7f6f5] p-7'}>
+        <Title text={name} size={'md'} className={'font-extrabold mb-1'} />
+
+        <Button
+          loading={loading}
+          onClick={() => onSubmit?.()}
+          className={'h-[55px] px-10 text-base rounded-[18px] w-full mt-10'}
+        >
+          Добавить в корзину за {price} ₽
+        </Button>
+      </div>
+    </div>
+  )
+}
